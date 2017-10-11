ff4066bf0f80c84344601e581b5cd3f5377272df
JCHR14 <juancalihernandez@gmail.com>
2017-10-10 19:19:27 -0600

    finalizando examen

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+
+var miinfo = {
+  cuenta:"0801-1994-17010",
+  nombre:"Juan Carlos Hernandez Reyes",
+  correo:"juancarlos.hernandez@bi-dss.com",
+  examen:"primer parcial"
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/suma', function(req, res, next) {
+  res.render('suma', { title: 'Express' });
+});
+
+router.post('/suma', function(req, res, next) {
+	var n1 = req.body.valor1;
+	var n2 = req.body.valor2;
+	var suma = parseInt(n1)+parseInt(n2);
+	res.json(suma);
+});
+
+router.get('/0801-1994-17010',
+   function(req,res,next){
+       res.json(miinfo);
+   }
+ );
+
 module.exports = router;
+  
